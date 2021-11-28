9e18016eb007764aff9895c9e23754176d8bbbd0
Alex811 <811alex@gmail.com>
2021-11-28 03:21:33 +0200

    ChunkList: add file output support

--- ChunkList.py
+++ ChunkList.py
@@ -2,15 +2,20 @@
 
 from anvil import Region
 from re import search
-from sys import stderr
+from sys import stdout, stderr
 from pathlib import Path
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from argparse import ArgumentParser, RawDescriptionHelpFormatter, FileType
+
+max_reprint_len = 0
+total_region_files = 0
+processed_region_files = 0
 
 def parse_args():
     parser = ArgumentParser(description='List chunks stored in the region (Anvil) files that exist in a directory.\nBy default, it lists fully generated chunks.', formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('regionpath', type=Path, help='Directory containing the region files to read (or a single region file).')
     parser.add_argument('--status', type=str, default='full', help='Chunk status filter (RegEx), by default "full".')
     parser.add_argument('--print-status', action='store_true', default=False, help='Also print each chunk\'s status.')
+    parser.add_argument('--output-file', nargs='?', type=FileType('w'), default=stdout, help='Output data to a file instead of stdout.')
     args = parser.parse_args()
     return args
 
@@ -39,7 +44,7 @@ def for_each_chunk_in_region(regionfile, consumer, nbt_data=False):
             for x in range(32):
                 for z in range(32):
                     try:
-                        consumer(chunk_func(x, z))
+                        consumer(chunk_func(x, z), regionfile.name)
                     except Exception as ex:
                         raise Exception('Failed at region "' + regionfile.name + '", chunk (' + str(x) + ',' + str(z) + ')!')
         return True
@@ -63,11 +68,14 @@ def for_each_chunk(regiondir, consumer, nbt_data=False):
     boolean
         True if at least one file was read, otherwise False
     """
-    files_found = False
-    for f in regiondir.glob('*.mca'):
-        files_found = True
-        for_each_chunk_in_region(f, consumer, nbt_data)
-    return files_found
+    global total_region_files, processed_region_files
+    total_region_files = len(list(regiondir.glob('*.mca')))
+    if total_region_files > 0:
+        for f in regiondir.glob('*.mca'):
+            processed_region_files += 1
+            for_each_chunk_in_region(f, consumer, nbt_data)
+        return True
+    return False
 
 def get_level(chunk):  # compat
     if chunk:
@@ -78,24 +86,44 @@ def get_level(chunk):  # compat
         return chunk
     return None
 
-def print_valid_chunk(chunk, status, print_status):
+def print_valid_chunk(chunk, output_file, output_is_file, status, print_status, region_filename):
+    global total_region_files, processed_region_files
     level = get_level(chunk)
     if level and search(status, level['Status'].value):
-        print(level['xPos'].valuestr() + ',' + level['zPos'].valuestr() + (',' + level['Status'].value if print_status else ''))
+        print(level['xPos'].valuestr() + ',' + level['zPos'].valuestr() + (',' + level['Status'].value if print_status else ''), file=output_file)
+        if output_is_file:
+            if total_region_files > 0:
+                reprint('Processed: ' + str(processed_region_files) + '/' + str(total_region_files) + '  |  Current: ' + region_filename)
+            else:
+                reprint('Processing: ' + region_filename)
+
+def reprint(val):
+    global max_reprint_len
+    print('\r' + val.ljust(max_reprint_len, ' '), end='')
+    if len(val) > max_reprint_len:
+        max_reprint_len = len(val)
 
 def error(err):
     print(err, file=stderr)
 
 def main():
     args = parse_args()
+    output_file = args.output_file
+    output_is_file = output_file and output_file != stdout
+    if output_is_file:
+        print('Writting to file: ' + output_file.name)
+    consumer = lambda chunk, region_filename: print_valid_chunk(chunk, output_file, output_is_file, args.status, args.print_status, region_filename)
     if args.regionpath.is_dir():
-        if not for_each_chunk(args.regionpath, lambda chunk: print_valid_chunk(chunk, args.status, args.print_status), True):
+        if not for_each_chunk(args.regionpath, consumer, True):
             error('No .mca files found in this directory.')
     elif args.regionpath.is_file():
-        if not for_each_chunk_in_region(args.regionpath, lambda chunk: print_valid_chunk(chunk, args.status, args.print_status), True):
+        if not for_each_chunk_in_region(args.regionpath, consumer, True):
             error('The file is not .mca')
     else:
         error('Unknown region path.')
+    if output_is_file:
+        output_file.close()
+        print('\nDone!')
 
 if __name__ == '__main__':
     main()
